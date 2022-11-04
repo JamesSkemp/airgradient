a1af56a1e47b42e00a3a801ec36c06a0adddc0c9
James Skemp <strivinglife@gmail.com>
2022-11-04 17:48:20 -0500

    Update API to add non-functional endpoint
    It also requires the colon be passed as the variable, instead of being part of the route.

--- AirGradientWebApi/Program.cs
+++ AirGradientWebApi/Program.cs
@@ -1,3 +1,6 @@
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -14,30 +22,22 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapPost("/sensors/airgradient{chipId}/measures", async (string chipId, AirGradientData data) => {
+    return Results.Ok();
+});
 
 app.Run();
 
-record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+record AirGradientData(
+    [property: JsonPropertyName("wifi")]
+    string Wifi,
+    [property: JsonPropertyName("rc02")]
+    string? Co2,
+    [property: JsonPropertyName("pm02")]
+    string? Pm02,
+    [property: JsonPropertyName("atmp")]
+    string Temperature,
+    [property: JsonPropertyName("rhum")]
+    string? Humidity
+    ) {
 }
